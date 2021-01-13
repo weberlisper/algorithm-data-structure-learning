4bec5d1e946919fd17b74e93f01b015783418d8d
lsp <317118524@qq.com>
2021-01-13 16:06:32 +0800

    finish stack and queue leetcode

---
+++ src/leetcode/implement_queue_using_stacks/MyQueue.java
@@ -0,0 +1,54 @@
+package leetcode.implement_queue_using_stacks;
+
+import java.util.Stack;
+
+class MyQueue {
+    private final Stack<Integer> stack1;
+    private final Stack<Integer> stack2;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    /**
+     * Push element x to the back of queue.
+     */
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    /**
+     * Removes the element from in front of queue and returns that element.
+     */
+    public int pop() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    /**
+     * Get the front element.
+     */
+    public int peek() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    /**
+     * Returns whether the queue is empty.
+     */
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
---
+++ src/leetcode/implement_queue_using_stacks/MyStack.java
@@ -0,0 +1,75 @@
+package leetcode.implement_queue_using_stacks;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+class MyStack {
+    private final Queue<Integer> queue1 = new ArrayDeque<>();
+    private final Queue<Integer> queue2 = new ArrayDeque<>();
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyStack() {
+
+    }
+
+    /**
+     * Push element x onto stack.
+     */
+    public void push(int x) {
+        if (!queue2.isEmpty()) {
+            queue2.add(x);
+            return;
+        }
+        queue1.add(x);
+    }
+
+    /**
+     * Removes the element on top of the stack and returns that element.
+     */
+    public int pop() {
+        if (!queue1.isEmpty()) {
+            while (queue1.size() > 1) {
+                queue2.add(queue1.remove());
+            }
+            return queue1.remove();
+        }
+        if (!queue2.isEmpty()) {
+            while (queue2.size() > 1) {
+                queue1.add(queue2.remove());
+            }
+            return queue2.remove();
+        }
+        return queue1.remove();
+    }
+
+    /**
+     * Get the top element.
+     */
+    public int top() {
+        int ret = 0;
+        if (!queue1.isEmpty()) {
+            while (!queue1.isEmpty()) {
+                ret = queue1.remove();
+                queue2.add(ret);
+            }
+            return ret;
+        }
+        if (!queue2.isEmpty()) {
+            while (!queue2.isEmpty()) {
+                ret = queue2.remove();
+                queue1.add(ret);
+            }
+            return ret;
+        }
+        return queue1.peek();
+    }
+
+    /**
+     * Returns whether the stack is empty.
+     */
+    public boolean empty() {
+        return queue1.isEmpty() && queue2.isEmpty();
+    }
+}
